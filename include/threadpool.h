0e8359e76e440c5dd9a14d377009e86cb1de4e76
NicoleRobin <lit050528@gmail.com>
2015-11-06 18:45:59 +0800

    ...

--- include/threadpool.h
+++ include/threadpool.h
@@ -1,9 +1,31 @@
 #ifndef __THREAD_POOL_H__
 #define __THREAD_POOL_H__
 
+#include <thread.h>
+
+#include <list>
+#include <map>
+
+
 namespace ZY
 {
-	
+	class ThreadPool
+	{
+	public:
+		ThreadPool();
+		~ThreadPool();
+		
+	public:
+		// initialize the thread pool
+		int Initialize();
+		
+		
+	private:
+		list<>
+		list<>
+		int m_nMaxThread;			// the max number of ThreadPool's threads
+		int m_nMinThread;			// the min number of ThreadPool's threads
+	};
 };
 
 #endif /* __THREAD_POOL_H__ */
